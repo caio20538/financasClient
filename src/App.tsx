0ffec284b1c1bd50726e37c56645e48a2c9a08a7
caio20538 <caio20538@gmail.com>
2025-08-12 15:47:19 -0300

    feat: initializing project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Hero } from './pages/Hero/Hero'
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Hero/Hero.tsx
@@ -0,0 +1,7 @@
+export const Hero:React.FC = () =>{
+    return(
+        <>
+            hero
+        </>
+    );
+}
