f0069dc58a7b585c71b7bd6878151a964f563b47
caio20538 <caio20538@gmail.com>
2025-08-15 15:01:42 -0300

    feat: add api link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api", // base da sua API
+  baseURL: "https://financasapi.onrender.com/api", // base da sua API
   headers: {
     "Content-Type": "application/json",
   },
